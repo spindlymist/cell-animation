70c0bac36bdc7a4a55e2ce0548d7f7a49aac8916
spindlymist <ocrobin@gmail.com>
2023-03-31 16:03:43 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const animationContainers = [...document.querySelectorAll('.animation-container')];
+animationContainers.forEach(initContainer);
+
+////////////////////////////////////////////////////////////////
+
+function initContainer(container) {
+    const grid = makeGrid(
+        container.dataset.gridRows,
+        container.dataset.gridCols,
+        container.dataset.animStagger ?? .1
+    );
+    grid.style.backgroundColor = "transparent";
+
+    container.append(grid);
+}
+
+function makeGrid(rows, cols, stagger) {
+    const grid = makeDiv('animation-grid');
+
+    const cellCount = rows * cols;
+
+    for(let i = 0; i < rows * cols; i++) {
+        const cell = makeDiv();
+        cell.style.setProperty("--anim-delay", (cellCount - (i + 1)) * stagger + "s");
+        grid.append(cell);
+    }
+
+    grid.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+
+    return grid;
+}
+
+////////////////////////////////////////////////////////////////
+// Utility
+
+function makeElement(tagName, className=undefined, innerHTML='') {
+    const el = document.createElement(tagName);
+    if(className) el.className = className;
+    el.innerHTML = innerHTML;
+    return el;
+}
+
+function makeDiv(className=undefined, innerHTML='') {
+    return makeElement('div', className, innerHTML);
+}
